bc6aed3f03724957f4644aa111cc5981a59a6ccc
EssouliHamza <ehamza.web@gmail.com>
2023-01-30 21:56:02 +0100

    Routing and User CRUD

---
+++ app/Controller/UserController.php
@@ -0,0 +1,106 @@
+<?php 
+class UserController extends Controller {
+    private $userModel;
+    public function __construct()
+    {
+        $this->userModel = $this->model('User');
+    }
+    public function processRequest(string $method, ?int $id = 0) : void 
+    {
+        if($id) $this->processResourceRequest($method, $id);
+        else
+            $this->processCollectionRequest($method);
+    }
+    private function processResourceRequest(string $method, string $id) : void
+    {
+        $user = $this->userModel->get($id);
+
+        if( ! $user ){
+            http_response_code(404);
+            echo json_encode(['message' => "User not found"]);
+            return;
+        }
+        switch($method) {
+            case 'GET':
+                echo json_encode($user);
+                break;
+            case 'PATCH':
+                $data = (array) json_decode(file_get_contents("php://input"), true);
+                $errors = $this->getValidationErrors($data);
+                if( ! empty($errors) ) {
+                    http_response_code(422);
+                    echo json_encode(['errors' => $errors]);
+                    break; 
+                }
+                $rows = $this->userModel->update($user ,$data);
+                echo json_encode([
+                    'message' => "User $id updated",
+                    'rows' => $rows
+                ]);
+                break;
+            default:
+                http_response_code(405);
+                header("Allow: GET, POST");
+        }
+        
+    }
+    private function processCollectionRequest(string $method) : void
+    {
+        switch($method) {
+            case 'GET':
+                print_r(json_encode($this->userModel->getAll()));
+                break;
+            case 'POST':
+                $data = (array) json_decode(file_get_contents("php://input"), true);
+                $errors = $this->getValidationErrors($data);
+                if( ! empty($errors) ) {
+                    http_response_code(422);
+                    echo json_encode(['errors' => $errors]);
+                    break; 
+                }
+                $id = $this->userModel->create($data);
+                http_response_code(201);
+                echo json_encode([
+                    'message' => "User created successfully",
+                    'id' => $id
+                ]);
+                break;
+            default:
+                http_response_code(405);
+                header("Allow: GET, POST");
+        }
+    }
+    private function getValidationErrors(array $data) : array
+    {
+        $errors = [];
+        $dataElements = array_keys($data);
+        foreach ($dataElements as $element)
+        {
+            if(empty($data[$element]))
+                $errors[$element] = $element . " is required";
+        }
+        // $dataElements = ["last name", "first name", "birth date", "nationality", "family situation", "address", "visa type", "start date", "end date", "travel type"];
+        // if (empty($data['last name']))
+        //     $errors['last name'] = "last name is required";
+        // if (empty($data['first name']))
+        //     $errors['first name'] = "first name is required";
+        // if (empty($data['birth date']))
+        //     $errors['birth date'] = "birth date  is required";
+        // if (empty($data['nationality']))
+        //     $errors['nationality'] = "nationality  is required";
+        // if (empty($data['family situation']))
+        //     $errors['family situation'] = "family situation  is required";
+        // if (empty($data['address']))
+        //     $errors['address'] = "address  is required";
+        // if (empty($data['visa type']))
+        //     $errors['visa type'] = "visa type  is required";
+        // if (empty($data['start date']))
+        //     $errors['start date'] = "start date  is required";
+        // if (empty($data['end date']))
+        //     $errors['end date'] = "end date  is required";
+        // if (empty($data['travel type']))
+        //     $errors['travel type'] = "travel type  is required";
+        return $errors;
+    }
+
+}
---
+++ app/Model/User.php
@@ -0,0 +1,78 @@
+<?php
+class User extends DataBase 
+{
+    private PDO $connection;
+    public function __construct() 
+    {
+        $this->connection = $this->connect();
+    }
+    public function getAll() : array 
+    {
+        $sql = "SELECT * 
+                FROM user";
+        $statement = $this->connection->query($sql);
+        return $statement->fetchAll();
+    }
+    public function create(array $data) : string
+    {
+        $sql = "INSERT INTO `user` 
+        (`last name`, `first name`, `birth date`, `nationality`, `family situation`, `address`, `visa type`, `start date`, `end date`, `travel type`, `travel number`)
+        VALUES (`:last name`, `:first name`, `:birth date`, `:nationality`, `:family situation`, `:address`, `:visa type`, `:start date`, `:end date`, `:travel type`, `:travel number`);";
+        $statement = $this->connection->prepare($sql);
+        $statement->bindParam('last name', $data['last name']);
+        $statement->bindParam('first name', $data['first name']);
+        $statement->bindParam('birth date', $data['birth date']);
+        $statement->bindParam('nationality', $data['nationality']);
+        $statement->bindParam('family situation', $data['family situation']);
+        $statement->bindParam('address', $data['address']);
+        $statement->bindParam('visa type', $data['visa type']);
+        $statement->bindParam('start date', $data['start date']);
+        $statement->bindParam('end date', $data['end date']);
+        $statement->bindParam('travel type', $data['travel type']);
+        $statement->bindParam('travel number', $data['travel number']);
+        $statement->execute();
+        return $this->connection->lastInsertId();
+    }
+
+    public function get(string $id) : array | false
+    {
+        $sql = "SELECT * 
+                FROM user
+                WHERE id = :id ;";
+        $statement = $this->connection->prepare($sql);
+        $statement->bindParam('id', $id, PDO::PARAM_INT);
+        $statement->execute();
+        return $statement->fetch();
+    }
+    public function update(array $current, array $new ) : int
+    {
+        $sql = "UPDATE `user` 
+                SET `last name` = :last_name,
+                    `first name` = :first_name,
+                    `birth date` = :birth_date,
+                    `nationality` = :nationality,
+                    `family situation` = :family_situation,
+                    `address` = :address,
+                    `visa type` = :visa_type,
+                    `start date` = :start_date,
+                    `end date` = :end_date,
+                    `travel type` = :travel_type,
+                    `travel number` = :travel_number
+                WHERE `user`.`id` = :id ;";
+        $statement = $this->connection->prepare($sql);
+        $statement->bindvalue('last_name', $new['last name'] ?? $current['last name'], PDO::PARAM_STR);
+        $statement->bindvalue('first_name', $new['first name'] ?? $current['first name']);
+        $statement->bindvalue('birth_date', $new['birth date'] ?? $current['birth date']);
+        $statement->bindvalue('nationality', $new['nationality'] ?? $current['nationality']);
+        $statement->bindvalue('family_situation', $new['family situation'] ?? $current['family situation']);
+        $statement->bindvalue('address', $new['address'] ?? $current['address']);
+        $statement->bindvalue('visa_type', $new['visa type'] ?? $current['visa type']);
+        $statement->bindvalue('start_date', $new['start date'] ?? $current['start date']);
+        $statement->bindvalue('end_date', $new['end date'] ?? $current['end date']);
+        $statement->bindvalue('travel_type', $new['travel type'] ?? $current['travel type']);
+        $statement->bindvalue('travel_number', $new['travel number'] ?? $current['travel number']);
+        $statement->bindValue(':id', $current['id'], PDO::PARAM_INT);
+        $statement->execute();
+        return $statement->rowCount();
+    }
+}
